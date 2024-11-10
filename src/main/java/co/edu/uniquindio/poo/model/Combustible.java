30d67455e7359a9cba6409e2577a8f30b4887afd
Juanfelon14x <juanfelon14x@gmail.com>
2024-11-10 15:36:39 -0500

    Add files via upload

--- src/main/java/co/edu/uniquindio/poo/model/Combustible.java
+++ src/main/java/co/edu/uniquindio/poo/model/Combustible.java
@@ -1,8 +1,44 @@
 package co.edu.uniquindio.poo.model;
 
-public enum Combustible {
-    GASOLINA,
-    DIESEL,
-    ELECTRICO,
-    HIBRIDO,
+public class Combustible {
+    public static class Gasolina extends Combustible {
+    }
+
+    public static class Diesel extends Combustible {
+    }
+
+    public static class Electrico extends Combustible {
+        private int autonomiacargcompleta;
+        private String tiempodecarga;
+
+        public Electrico(int autonomiacargcompleta, String tiempodecarga) {
+            this.autonomiacargcompleta = autonomiacargcompleta;
+            this.tiempodecarga = tiempodecarga;
+        }
+
+        public int getAutonomiacargcompleta() {
+            return autonomiacargcompleta;
+        }
+
+        public void setAutonomiacargcompleta(int autonomiacargcompleta) {
+            this.autonomiacargcompleta = autonomiacargcompleta;
+        }
+
+        public String getTiempodecarga() {
+            return tiempodecarga;
+        }
+
+        public void setTiempodecarga(String tiempodecarga) {
+            this.tiempodecarga = tiempodecarga;
+        }
+    }
+
+    public static class Hibrido extends Combustible {
+        public static class HibridoNoEnchufable extends Hibrido {
+            public static final HibridoNoEnchufable HIBRIDO_LIGERO = new HibridoNoEnchufable();
+            public static final HibridoNoEnchufable HIBRIDO_NO_LIGERO = new HibridoNoEnchufable();
+        }
+
+        public static final Hibrido ENCHUFABLE = new Hibrido();
+    }
 }
